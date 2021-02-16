65a7ee0fca81e22cb149034c3893164218608a92
Robert Obi <robobi97@googlemail.com>
2021-02-16 18:23:50 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,7 +6,7 @@
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |name, age|
-  "Happy birthday #{name}, you are now #{age}"
+  "Happy birthday #{name}! You are now #{age} years old!"
  end
 end
 
