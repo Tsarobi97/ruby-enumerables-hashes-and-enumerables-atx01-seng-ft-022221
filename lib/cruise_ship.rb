5e71946d03d0d281f46e6001aeda6616cb3b8a95
Robert Obi <robobi97@googlemail.com>
2021-02-16 18:03:45 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -10,7 +10,7 @@ def select_winner(passengers)
   winner = ""
   passengers.each do |key, value|
     if key == :suite_a && value[0] == "A"
-      winner = #{value}
+      winner = value
        value
   end
  end
